cd15d0fc239539a7275aedf880c8da2cf05227a7
Йоно Йонов <redyono2@gmail.com>
2025-08-06 23:58:31 +0300

    Added HomeCOntroller Unit test

---
+++ SSKBooks1.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SSKBooks.Models;
+using SSKBooks1.Controllers;
+using SSKBooks1.Models;
+using System.Diagnostics;
+using Xunit;
+
+namespace SSKBooks1.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly Mock<ILogger<HomeController>> _mockLogger;
+        private readonly HomeController _controller;
+
+        public HomeControllerTests()
+        {
+            _mockLogger = new Mock<ILogger<HomeController>>();
+            _controller = new HomeController(_mockLogger.Object);
+        }
+
+        [Fact]
+        public void Index_ReturnsView()
+        {
+            var result = _controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Privacy_ReturnsView()
+        {
+            var result = _controller.Privacy();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void AccessDenied_ReturnsView()
+        {
+            var result = _controller.AccessDenied();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Error_ReturnsErrorViewModel()
+        {
+            // Arrange
+            var controllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+            _controller.ControllerContext = controllerContext;
+
+            // Act
+            var result = _controller.Error();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<ErrorViewModel>(viewResult.Model);
+            Assert.False(string.IsNullOrEmpty(model.RequestId));
+        }
+    }
+}
