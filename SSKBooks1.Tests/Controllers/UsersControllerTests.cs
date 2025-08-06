7d8af200ab97d532b11f9f1e51e95f53dbac9eeb
Йоно Йонов <redyono2@gmail.com>
2025-08-06 23:53:03 +0300

    Added unit testing for UsersController

---
+++ SSKBooks1.Tests/Controllers/UsersControllerTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SSKBooks1.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SSKBooks1.Tests.Controllers
+{
+    public class UsersControllerTests
+    {
+        private readonly Mock<UserManager<IdentityUser>> _mockUserManager;
+        private readonly Mock<RoleManager<IdentityRole>> _mockRoleManager;
+        private readonly UsersController _controller;
+
+        public UsersControllerTests()
+        {
+            var userStore = new Mock<IUserStore<IdentityUser>>();
+            _mockUserManager = new Mock<UserManager<IdentityUser>>(userStore.Object, null, null, null, null, null, null, null, null);
+
+            var roleStore = new Mock<IRoleStore<IdentityRole>>();
+            _mockRoleManager = new Mock<RoleManager<IdentityRole>>(roleStore.Object, null, null, null, null);
+
+            _controller = new UsersController(_mockUserManager.Object, _mockRoleManager.Object);
+        }
+
+        [Fact]
+        public async Task Index_ReturnsAllUsers()
+        {
+            var users = new List<IdentityUser>
+            {
+                new IdentityUser { UserName = "user1" },
+                new IdentityUser { UserName = "user2" }
+            };
+
+            _mockUserManager.Setup(u => u.Users).Returns(users.AsQueryable());
+
+            var result = _controller.Index();
+
+            var view = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<IdentityUser>>(view.Model);
+            Assert.Equal(2, model.Count());
+        }
+
+        [Fact]
+        public async Task Details_NullId_ReturnsNotFound()
+        {
+            var result = await _controller.Details(null);
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task Details_InvalidUser_ReturnsNotFound()
+        {
+            _mockUserManager.Setup(u => u.FindByIdAsync("bad-id")).ReturnsAsync((IdentityUser)null);
+
+            var result = await _controller.Details("bad-id");
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task Details_ValidUser_ReturnsView()
+        {
+            var user = new IdentityUser { Id = "1", UserName = "user1" };
+            _mockUserManager.Setup(u => u.FindByIdAsync("1")).ReturnsAsync(user);
+
+            var result = await _controller.Details("1");
+
+            var view = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<IdentityUser>(view.Model);
+            Assert.Equal("user1", model.UserName);
+        }
+
+        [Fact]
+        public async Task DeleteConfirmed_ValidUser_DeletesAndRedirects()
+        {
+            var user = new IdentityUser { Id = "1", UserName = "user1" };
+            _mockUserManager.Setup(u => u.FindByIdAsync("1")).ReturnsAsync(user);
+            _mockUserManager.Setup(u => u.DeleteAsync(user)).ReturnsAsync(IdentityResult.Success);
+
+            var result = await _controller.DeleteConfirmed("1");
+
+            var redirect = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Index", redirect.ActionName);
+        }
+
+        [Fact]
+        public async Task DeleteConfirmed_UserNotFound_ReturnsNotFound()
+        {
+            _mockUserManager.Setup(u => u.FindByIdAsync("x")).ReturnsAsync((IdentityUser)null);
+
+            var result = await _controller.DeleteConfirmed("x");
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+    }
+}
--- SSKBooks1/Controllers/UsersController.cs
+++ SSKBooks1/Controllers/UsersController.cs
@@ -1,23 +1,70 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace SSKBooks.Controllers
+namespace SSKBooks1.Controllers
 {
-    [Authorize(Roles = "Admin")] // ✅ Only Admins can access this controller
+    [Authorize(Roles = "Admin")]
     public class UsersController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UsersController(UserManager<IdentityUser> userManager)
+        public UsersController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
+        // GET: /Users
         public IActionResult Index()
         {
             var users = _userManager.Users.ToList();
             return View(users);
+        }
+
+        // GET: /Users/Details/{id}
+        public async Task<IActionResult> Details(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return NotFound();
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            return View(user);
+        }
+
+        // GET: /Users/Delete/{id}
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                return NotFound();
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            return View(user);
+        }
+
+        // POST: /Users/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+                return NotFound();
+
+            var result = await _userManager.DeleteAsync(user);
+            if (!result.Succeeded)
+                return BadRequest();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
